7e873fd5136274565cc8de1a1c71d701ee18f81a
jeremyHarrell <hillvulf@gmail.com>
2023-10-04 16:08:10 -0500

    solution for create registration form complete

--- src/App.js
+++ src/App.js
@@ -1,34 +1,124 @@
 import "./App.css";
-import DessertsList from "./DessertsList";
+import { useState } from "react";
+import { validateEmail } from "./utils";
 
-const desserts = [
-  {
-    name: "Chocolate Cake",
-    calories: 400,
-    createdAt: "2022-09-01",
-  },
-  {
-    name: "Ice Cream",
-    calories: 200,
-    createdAt: "2022-01-02",
-  },
-  {
-    name: "Tiramisu",
-    calories: 300,
-    createdAt: "2021-10-03",
-  },
-  {
-    name: "Cheesecake",
-    calories: 600,
-    createdAt: "2022-01-04",
-  },
-];
+const PasswordErrorMessage = () => {
+    return (
+      <p className="FieldError">Password should have at least 8 characters</p>
+    )
+
+};
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState({
+    value: "",
+    isTouched: false,
+  });
+  const [role, setRole] = useState("role");
+
+  const getIsFormValid = () => {
+    return (
+      firstName &&
+      validateEmail(email) &&
+      password.value.length > 7 &&
+      role !== 'role')
+  };
+
+  const clearForm = (e) => {
+    setFirstName("")
+    setLastName("")
+    setEmail("")
+    setPassword({
+      value: "",
+      isTouched: false
+    })
+    setRole("role")
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    alert("Account created!");
+    clearForm();
+  };
+
   return (
     <div className="App">
-      <h2>List of low calorie desserts:</h2>
-      <DessertsList data={desserts} />
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <h2>Sign Up</h2>
+          <div className="Field">
+            <label>
+              First name <sup>*</sup>
+            </label>
+            <input
+              id="firstName"
+              placeholder="First name"
+              type="text"
+              value={firstName}
+              onChange={e => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className="Field">
+            <label>
+              Last name
+            </label>
+            <input
+              id="lastName"
+              placeholder="Last name"
+              type="text"
+              value={lastName}
+              onChange={e => setLastName(e.target.value)}/>
+          </div>
+          <div className="Field">
+            <label >
+              Email address <sup>*</sup>
+            </label>
+            <input
+              id="email"
+              placeholder="Email address"
+              type="text"
+              value={email}
+              onChange={e => setEmail(e.target.value)}/>
+          </div>
+          <div className="Field">
+            <label>
+              Password <sup>*</sup>
+            </label>
+            <input
+              id="password"
+              type="password"
+              value={password.value}
+              onChange={(e) => {
+                setPassword({
+                  ...password, value: e.target.value})
+                }}
+                onBlur={() => {
+                  setPassword({ ...password, isTouched: true })
+                }}
+                placeholder="Password"
+            />
+            {password.isTouched && password.value.length < 8 ? (
+              <PasswordErrorMessage />
+            ) : null}
+          </div>
+          <div className="Field">
+            <label>
+              Role <sup>*</sup>
+            </label>
+            <select value={role} onChange={e => setRole(e.target.value)}>
+              <option value="role">Role</option>
+              <option value="individual">Individual</option>
+              <option value="business">Business</option>
+            </select>
+          </div>
+          <button type="submit" disabled={!getIsFormValid()}>
+            Create account
+          </button>
+        </fieldset>
+      </form>
     </div>
   );
 }
--- src/DessertsList.js
+++ src/DessertsList.js
@@ -2,8 +2,11 @@ function DessertsList(props) {
   const filterDesserts = props.data.filter((dessert) => dessert.calories < 501)
   .sort((a,b) => {return a.calories - b.calories })
   .map(dessert => {
-    const itemText = `${dessert.name} - ${dessert.calories} cal.`
-    return <li>{itemText}</li>
+  return (
+    <li>
+      {dessert.name} - {dessert.calories} cal
+    </li>
+  )
   })
   return (
     <ul>
