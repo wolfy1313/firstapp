23ea173b02c187cb2ce15eac3d0bc8c2590e1f5f
jeremyHarrell <hillvulf@gmail.com>
2023-10-09 15:31:40 -0500

    useState hook exercise complete

--- src/App.js
+++ src/App.js
@@ -1,88 +1,139 @@
-import "./App.css";
-import { ThemeProvider, useTheme } from "./ThemeContext";
-import Switch from "./Switch";
+import { useState } from "react";
 
-const Title = ({ children }) => {
-  const { theme } = useTheme();
-  return (
-    <h2
-      style={{
-        color: theme === "light" ? "black" : "white",
-      }}
-    >
-      {children}
-    </h2>
+export default function App() {
+  const [giftCard, setGiftCard] = useState(
+    {
+        firstName: "Jennifer",
+        lastName: "Smith",
+        text: "Free dinner for 4 guests",
+        valid: true,
+        instructions: "To use your coupon, click the button below.",
+    }
   );
-};
 
-const Paragraph = ({ children }) => {
-  const { theme } = useTheme();
-  return (
-    <p
-      style={{
-        color: theme === "light" ? "black" : "white",
-      }}
-    >
-      {children}
-    </p>
-  );
-};
+  function spendGiftCard(e) {
+    setGiftCard((prevState) => {
+      return ({...prevState, 
+        text: "Your coupon has been used.",
+        valid: false,
+        instructions: "Please visit our restaurant to renew your gift card."  
+      })
+    })
 
-const Content = () => {
-  return (
-    <div>
-      <Paragraph>
-        We are a pizza loving family. And for years, I searched and searched and
-        searched for the perfect pizza dough recipe. I tried dozens, or more.
-        And while some were good, none of them were that recipe that would
-        make me stop trying all of the others.
-      </Paragraph>
-    </div>
-  );
-};
+  }
 
-const Header = () => {
   return (
-    <header>
-      <Title>Little Lemon 🍕</Title>
-      <Switch />
-    </header>
-  );
-};
-
-const Page = () => {
-  return (
-    <div className="Page">
-      <Title>When it comes to dough</Title>
-      <Content />
-    </div>
-  );
-};
-
-function App() {
-  const { theme } = useTheme();
-  return (
-    <div
-      className="App"
-      style={{
-        backgroundColor: theme === "light" ? "white" : "black",
-      }}
-    >
-      <Header />
-      <Page />
+    <div style={{padding: '40px'}}>
+      <h1>
+        Gift Card Page
+      </h1>
+      <h2>
+        Customer: {giftCard.firstName} {giftCard.lastName}
+      </h2>
+      <h3>
+        {giftCard.text}
+      </h3>
+      <p>
+        {giftCard.instructions}
+      </p>
+      {
+        giftCard.valid && (
+          <button onClick={spendGiftCard}>
+            Spend Gift Card
+          </button>
+        )
+      }
     </div>
   );
 }
 
-function Root() {
-  return (
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  );
-}
 
-export default Root;
+// import "./App.css";
+// import { ThemeProvider, useTheme } from "./ThemeContext";
+// import Switch from "./Switch";
+
+// const Title = ({ children }) => {
+//   const { theme } = useTheme();
+//   return (
+//     <h2
+//       style={{
+//         color: theme === "light" ? "black" : "white",
+//       }}
+//     >
+//       {children}
+//     </h2>
+//   );
+// };
+
+// const Paragraph = ({ children }) => {
+//   const { theme } = useTheme();
+//   return (
+//     <p
+//       style={{
+//         color: theme === "light" ? "black" : "white",
+//       }}
+//     >
+//       {children}
+//     </p>
+//   );
+// };
+
+// const Content = () => {
+//   return (
+//     <div>
+//       <Paragraph>
+//         We are a pizza loving family. And for years, I searched and searched and
+//         searched for the perfect pizza dough recipe. I tried dozens, or more.
+//         And while some were good, none of them were that recipe that would
+//         make me stop trying all of the others.
+//       </Paragraph>
+//     </div>
+//   );
+// };
+
+// const Header = () => {
+//   return (
+//     <header>
+//       <Title>Little Lemon 🍕</Title>
+//       <Switch />
+//     </header>
+//   );
+// };
+
+// const Page = () => {
+//   return (
+//     <div className="Page">
+//       <Title>When it comes to dough</Title>
+//       <Content />
+//     </div>
+//   );
+// };
+
+// function App() {
+//   const { theme } = useTheme();
+//   return (
+    
+//     <div
+//       className="App"
+//       style={{
+//         backgroundColor: theme === "light" ? "white" : "black",
+//       }}
+//     >
+//       <Header />
+//       <Page />
+//     </div>
+//   );
+// }
+
+// function Root() {
+//   return (
+//     <ThemeProvider>
+//       <App />
+//     </ThemeProvider>
+//   );
+// }
+
+// export default Root;
 
 
 
